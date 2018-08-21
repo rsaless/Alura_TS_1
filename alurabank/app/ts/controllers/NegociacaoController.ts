0d7355822dca6e16d890b53fa4287008865a49f5
Rsaless <rspavarina@gmail.com>
2018-08-21 00:50:12 -0300

    Preparando o projeto, 25%

---
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,20 @@
+class NegociacaoController {
+    private _inputData;
+    private _inputQuantidade;
+    private _inputValor;
+
+    constructor(){
+        this._inputData = document.querySelector("#data");
+        this._inputQuantidade = document.querySelector("#quantidade");
+        this._inputValor = document.querySelector("#valor");
+    }
+
+    adiciona(evento){
+        event.preventDefault();
+        const negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value            
+        );
+    }
+}
