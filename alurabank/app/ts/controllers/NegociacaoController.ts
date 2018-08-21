88659497149774a3e1f99fb0ac55fd6b4f045982
Rsaless <rspavarina@gmail.com>
2018-08-21 12:55:35 -0300

    81% concluído

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -4,6 +4,8 @@ class NegociacaoController {
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
+    
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector("#data");
         this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade");
@@ -21,6 +23,7 @@ class NegociacaoController {
         );
 
         this._negociacoes.adiciona(negociacao);
-
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso')
     }
 }
